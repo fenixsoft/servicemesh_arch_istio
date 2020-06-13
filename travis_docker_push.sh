8523ecd1c7896704756d492b5170f78df3996a09
icyfenix <icyfenix@gmail.com>
2020-06-13 20:05:35 +0800

    init commit

--- travis_docker_push.sh
+++ travis_docker_push.sh
@@ -2,31 +2,31 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 # frontend
-cd ./bookstore-serivcemesh-domain-frontend/
+cd ./bookstore-servicemesh-domain-frontend/
 docker build -t bookstore-istio-domain-frontend .
 docker tag bookstore-istio-domain-frontend $DOCKER_USERNAME/bookstore-istio-platform-gateway
 cd ..
 
 # authentication
-cd ./bookstore-serivcemesh-domain-authentication/
+cd ./bookstore-servicemesh-domain-authentication/
 docker build -t bookstore-istio-domain-authentication .
 docker tag bookstore-istio-domain-authentication $DOCKER_USERNAME/bookstore-istio-domain-authentication
 cd ..
 
 # account
-cd ./bookstore-serivcemesh-domain-account/
+cd ./bookstore-servicemesh-domain-account/
 docker build -t bookstore-istio-domain-account .
 docker tag bookstore-istio-domain-account $DOCKER_USERNAME/bookstore-istio-domain-account
 cd ..
 
 # warehouse
-cd ./bookstore-serivcemesh-domain-warehouse/
+cd ./bookstore-servicemesh-domain-warehouse/
 docker build -t bookstore-istio-domain-warehouse .
 docker tag bookstore-istio-domain-warehouse $DOCKER_USERNAME/bookstore-istio-domain-warehouse
 cd ..
 
 # payment
-cd ./bookstore-serivcemesh-domain-payment/
+cd ./bookstore-servicemesh-domain-payment/
 docker build -t bookstore-istio-domain-payment .
 docker tag bookstore-istio-domain-payment $DOCKER_USERNAME/bookstore-istio-domain-payment
 cd ..
